31bad2c14d18c1ae1354bed433da525f0974399a
MaiconMaciel <maicon.costa1331@gmail.com>
2025-12-30 12:00:29 -0300

    more fixes

--- config/spark_settings.py
+++ config/spark_settings.py
@@ -1,13 +1,23 @@
+import os
 from pyspark.sql import SparkSession
-from delta import configure_spark_with_delta_pip
 from delta.tables import *
 
+def sparkSessionBuilder():
 
-def sparkSessionBuilder(app_name="spark session"):
-    builder = SparkSession.builder \
-      .appName(app_name) \
-      .master("local[*]") \
-      .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
-      .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+    is_databricks = os.getenv("DATABRICKS_RUNTIME_VERSION") is not None
 
-    return  configure_spark_with_delta_pip(builder).getOrCreate()
+    if is_databricks:
+        # Get databricks session
+        return SparkSession.builder.getOrCreate()
+
+    else:
+        # local development
+        return (
+            SparkSession.builder
+            .appName("OlistLakehouse")
+            .master("local[*]")  # Use all cores
+            .config("spark.jars.packages", "io.delta:delta-core_2.12:2.4.0")  # Enable Delta
+            .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+            .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+            .getOrCreate()
+        )
